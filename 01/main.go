0a481fb40d2586fc84dfec38a2781c3f57236c7d
sadrishehu <sadrishehu@gmail.com>
2023-12-01 08:21:38 +0100

    AOC-003: first day puzzle

--- 01/main.go
+++ 01/main.go
@@ -1,6 +1,116 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var (
+	digitsMap = map[string]int{
+		"one":   1,
+		"two":   2,
+		"three": 3,
+		"four":  4,
+		"five":  5,
+		"six":   6,
+		"seven": 7,
+		"eight": 8,
+		"nine":  9,
+	}
+
+	digits = []string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+)
+
 func main() {
-	// START OMIT
-	// END OMIT
+	day01a()
+	day01b()
+}
+
+func day01a() {
+	data := readFile()
+
+	coordinates := []int{}
+	for _, line := range data {
+		num := []int{}
+		for _, char := range line {
+			number, err := strconv.Atoi(string(char))
+			if err != nil {
+				continue
+			}
+			num = append(num, number)
+		}
+
+		coordinates = append(coordinates, buildCoordinates(num))
+	}
+
+	fmt.Println("Total:", calculateTotal(coordinates))
+}
+
+func day01b() {
+	data := readFile()
+
+	coordinates := []int{}
+	for _, line := range data {
+		num := []int{}
+
+		current := ""
+		for _, char := range line {
+			current = current + string(char)
+			for _, digit := range digits {
+				if strings.Contains(current, digit) {
+					num = append(num, digitsMap[digit])
+					current = string(char)
+				}
+			}
+
+			number, err := strconv.Atoi(string(char))
+			if err != nil {
+				continue
+			}
+			num = append(num, number)
+		}
+
+		coordinates = append(coordinates, buildCoordinates(num))
+	}
+
+	fmt.Println("Total:", calculateTotal(coordinates))
+}
+
+func readFile() []string {
+	input, err := os.ReadFile("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return strings.Split(string(input), "\n")
+}
+
+func buildCoordinates(num []int) int {
+	var coordinate string
+	if len(num) == 1 {
+		coordinate = fmt.Sprintf("%d%d", num[0], num[0])
+	} else if len(num) >= 2 {
+		coordinate = fmt.Sprintf("%d%d", num[0], num[len(num)-1])
+	} else {
+		return 0
+	}
+
+	crd, err := strconv.Atoi(coordinate)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return crd
+}
+
+func calculateTotal(coordinates []int) int {
+	total := 0
+	for _, coordinate := range coordinates {
+		total += coordinate
+	}
+
+	return total
 }
