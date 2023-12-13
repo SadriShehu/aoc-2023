53c906c9950bd61526abf8804d964c2f7c65aa21
sadrishehu <sadrishehu@gmail.com>
2023-12-13 16:21:26 +0100

    AOC-015: solved second part of day eight

--- 08/main.go
+++ 08/main.go
@@ -9,19 +9,112 @@ import (
 	"time"
 )
 
+var (
+	instructions      string
+	mapStepsLeft      = make(map[string]string)
+	mapStepsRight     = make(map[string]string)
+	startingPositions []string
+)
+
 func main() {
+	readFile()
+
 	start := time.Now()
 	day08a()
+	fmt.Printf("Execution time: %s\n", time.Since(start))
+
+	start = time.Now()
+	day08b()
 	fmt.Printf("Execution time: %s\n", time.Since(start))
 }
 
 func day08a() {
-	input := readFile()
+	currentStep := "AAA"
+	terminated := false
+	steps := 0
+	for !terminated {
+		for _, step := range instructions {
+			if currentStep == "ZZZ" {
+				terminated = true
+				break
+			}
 
-	var instructions string
-	var mapStepsLeft = make(map[string]string)
-	var mapStepsRight = make(map[string]string)
-	stepsRegex := regexp.MustCompile(`([A-Z]{3}) = \(([A-Z]{3}), ([A-Z]{3})\)`)
+			if step == 'L' {
+				currentStep = mapStepsLeft[currentStep]
+				steps++
+
+				continue
+			}
+
+			currentStep = mapStepsRight[currentStep]
+			steps++
+		}
+	}
+
+	fmt.Println("Part One:", steps)
+}
+
+func day08b() {
+	output := 0
+	if len(startingPositions) > 0 {
+		ghostSteps := []int{}
+		for _, node := range startingPositions {
+			steps := 0
+			terminated := false
+			for !terminated {
+				for _, direction := range instructions {
+					if node[2] == 'Z' {
+						terminated = true
+						break
+					}
+
+					if direction == 'L' {
+						node = mapStepsLeft[node]
+						steps++
+						continue
+					}
+
+					node = mapStepsRight[node]
+					steps++
+				}
+			}
+
+			ghostSteps = append(ghostSteps, steps)
+		}
+
+		result := ghostSteps[0]
+		for i := 1; i < len(ghostSteps); i++ {
+			result = leastCommonMultiple(result, ghostSteps[i])
+		}
+		output = result
+	}
+
+	fmt.Println("Part Two:", output)
+}
+
+func greatestCommonDivisor(a, b int) int {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+
+	return a
+}
+
+func leastCommonMultiple(a, b int) int {
+	return a * b / greatestCommonDivisor(a, b)
+}
+
+func readFile() {
+	file, err := os.ReadFile("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	input := strings.Split(string(file), "\n")
+
+	stepsRegex := regexp.MustCompile(`([A-Z0-9]{3}) = \(([A-Z0-9]{3}), ([A-Z0-9]{3})\)`)
 
 	for i, line := range input {
 		if i == 0 {
@@ -33,43 +126,9 @@ func day08a() {
 		if len(groups) > 0 {
 			mapStepsLeft[groups[1]] = groups[2]
 			mapStepsRight[groups[1]] = groups[3]
+			if groups[1][2] == 'A' {
+				startingPositions = append(startingPositions, groups[1])
+			}
 		}
 	}
-
-	currentStep := "AAA"
-	steps := 0
-	i := 0
-	for {
-		if i == len(instructions) && currentStep != "ZZZ" {
-			i = 0
-		}
-
-		if currentStep == "ZZZ" {
-			break
-		}
-
-		step := instructions[i]
-
-		if step == 'L' {
-			currentStep = mapStepsLeft[currentStep]
-			steps++
-			i++
-
-			continue
-		}
-		currentStep = mapStepsRight[currentStep]
-		steps++
-		i++
-	}
-
-	fmt.Println(steps)
-}
-
-func readFile() []string {
-	input, err := os.ReadFile("input.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return strings.Split(string(input), "\n")
 }
